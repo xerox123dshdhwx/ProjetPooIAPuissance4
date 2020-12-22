c04f8a0abc7246ac7acbe67aa38b8199f5cb8d33
Boughanmi Rami <rami.boughanmi@etu.u-pec.fr>
2020-12-22 16:56:55 +0100

    classe qui contient la reference du fichier txt

---
+++ Date_base.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.*;
+
+
+public class Date_base {
+
+    private  File  fichier = new File("leFichier.txt");
+
+    public  File getFichier() {
+        return fichier;
+    }
+}
+
+
+
+
+
