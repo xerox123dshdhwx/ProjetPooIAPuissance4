0e9039f8654e08994e38f1dcaa7a76cb4efa31ca
Ait Khelifa Tanina <tanina.ait-khelifa@etu.u-pec.fr>
2020-12-23 17:01:02 +0100

    Replace Partie_IA.java : inclusion de l'énumération

--- Partie_IA.java
+++ Partie_IA.java
@@ -10,32 +10,32 @@ import java.util.Random;
 
 public class Partie_IA {
     private Partie_humain partie;
-    private int niveau_IA;
+    private Niveau lvl;
 
 
     /** Constructeur d'objet sans paramètres de la classe Partie_IA, permettant de créer un partie
      * sur une grille 6x7, le nom du joueur est initialisé à "Joueur 1"
      *
-     * @param lvl le niveau de difficulté de l'IA
-     * @param num le numéro de sauvegarde de la partie
+     * @param niv Niveau le niveau de difficulté de l'IA
+     * @param num int, le numéro de sauvegarde de la partie
      * */
 
-    public Partie_IA(int lvl, int num){
+    public Partie_IA(Niveau niv, int num){
         partie = new Partie_humain("Joueur 1", "IA", num);
-        niveau_IA = lvl;
+        lvl = niv;
     }
 
     /** Constructeur d'objet de la classe Partie_IA permettant de créer un partie
      * sur une grille 6x7, le joueur peut alors choisir son nom
      *
      * @param j1 String, correspond au nom du joueur
-     * @param lvl le niveau de difficulté de l'IA
-     * @param num le numéro de sauvegarde de la partie
+     * @param niv Niveau, le niveau de difficulté de l'IA
+     * @param num int, le numéro de sauvegarde de la partie
      */
 
-    public Partie_IA(String j1, int lvl, int num){
+    public Partie_IA(String j1, Niveau niv, int num){
         partie = new Partie_humain(j1, "IA", num);
-        niveau_IA = lvl;
+        lvl = niv;
     }
 
     /** Constructeur d'objet de la classe Partie_IA permettant de choisir la taille de la grille
@@ -43,13 +43,13 @@ public class Partie_IA {
      *
      * @param l int, correspond à la hauteur de la grille
      * @param c int, correspond à la largeur de la grille
-     * @param lvl le niveau de difficulté de l'IA
+     * @param niv le niveau de difficulté de l'IA
      * @param num le numéro de sauvegarde de la partie
      */
 
-    public Partie_IA(int l, int c, int lvl, int num){
+    public Partie_IA(int l, int c, Niveau niv, int num){
         partie = new Partie_humain(l, c,"Joueur 1", "IA", num);
-        niveau_IA = lvl;
+        lvl = niv;
     }
 
     /** Constructeur d'objet de la classe Partie_IA permettant de choisir la taille de la grille
@@ -58,13 +58,13 @@ public class Partie_IA {
      * @param l int, correspond à la hauteur de la grille
      * @param c int, correspond à la largeur de la grille
      * @param j1 String, correspond au nom du joueur
-     * @param lvl le niveau de difficulté de l'IA
-     * @param num le numéro de sauvegarde de la partie
+     * @param niv Niveau, le niveau de difficulté de l'IA
+     * @param num int, le numéro de sauvegarde de la partie
      */
 
-    public Partie_IA(int l, int c, String j1, int lvl, int num){
+    public Partie_IA(int l, int c, String j1, Niveau niv, int num){
         partie = new Partie_humain(l, c, j1, "IA", num);
-        niveau_IA = lvl;
+        lvl = niv;
     }
 
     /** Fonction qui permet à l'IA naïve de poser un pion aléatoirement dans la grille de jeu
@@ -100,11 +100,15 @@ public class Partie_IA {
             Deroulement_partie.add_pion(game.partie, 1);
         } else {
             System.out.println("Au tour de l'IA de jouer !");
-            if(game.niveau_IA == 0) {
+            if(game.lvl == Niveau.FACILE) {
                 add_pion_naive(game);
             }
         }
         game.partie.setNb_coups(game.partie.getNb_coups() + 1);
         return Deroulement_partie.save(game.partie);
+    }
+
+    public void genere_possi (Partie_IA game){
+
     }
 }
