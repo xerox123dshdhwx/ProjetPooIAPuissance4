b202760a079db7ae067847bc4e2a8bb5c85f1ccb
Ait Khelifa Tanina <tanina.ait-khelifa@etu.u-pec.fr>
2020-12-31 15:29:28 +0100

    Update Partie_humain.java

--- Partie_humain.java
+++ Partie_humain.java
@@ -3,7 +3,7 @@
  *
  *
  * @author AIT KHELIFA Tanina & BOUGHANMI Rami
- * @version 11/11/2020
+ * @version 31/12/2020
  */
 
 public class Partie_humain {
@@ -21,7 +21,6 @@ public class Partie_humain {
      *
      * @param p Parie_humain, la partie à copier
      */
-
     public Partie_humain(Partie_humain p){
         lignes = p.lignes;
         colonnes = p.colonnes;
@@ -35,10 +34,10 @@ public class Partie_humain {
         num_partie = p.num_partie;
     }
 
+
     /** Constructeur d'objet sans paramètres de la classe Partie_humain, permettant de créer un partie
      * sur une grille 6x7, les noms des joueurs sont initialisés à "Joueur 1" et "Joueur 2"
-     * */
-
+     */
     public Partie_humain(int n){
         lignes = 6;
         colonnes = 7;
@@ -47,13 +46,13 @@ public class Partie_humain {
         num_partie = n;
     }
 
+
     /** Constructeur d'objet de la classe Partie_humain permettant de créer un partie
      * sur une grille 6x7, les joueurs peuvent alors choisir leur noms
      *
      * @param j1 String, correspond au nom du joueur 1
      * @param j2 String, correspond au nom du joueur 2
      */
-
     public Partie_humain(String j1, String j2, int n){
         lignes = 6;
         colonnes = 7;
@@ -64,13 +63,13 @@ public class Partie_humain {
         num_partie = n;
     }
 
+
     /** Constructeur d'objet de la classe Partie_humain permettant de choisir la taille de la grille
      * de jeu, les noms des joueurs sont initialisés à "Joueur 1" et "Joueur 2"
      *
      * @param l int, correspond à la hauteur de la grille
      * @param c int, correspond à la largeur de la grille
      */
-
     public Partie_humain(int l, int c, int n){
         tab_de_jeu = new int[l][c];
         lignes = l;
@@ -78,6 +77,7 @@ public class Partie_humain {
         nb_coups = 0;
         num_partie = n;
     }
+
 
     /** Constructeur d'objet de la classe Partie_humain permettant de choisir la taille de la grille
      * de jeu, les joueurs peuvent alors choisir leur noms
@@ -87,7 +87,6 @@ public class Partie_humain {
      * @param j1 String, correspond au nom du joueur 1
      * @param j2 String, correspond au nom du joueur 2
      */
-
     public Partie_humain(int l, int c, String j1, String j2, int n){
         tab_de_jeu = new int[l][c];
         lignes = l;
@@ -115,6 +114,7 @@ public class Partie_humain {
         this.tab_de_jeu = tab;
     }
 
+
     /** Fonction retournant le nom du premier joueur d'une partie entre deux humains
      *
      * @return String correspondant au champ joueur_1 d'un objet de classe Partie_humain
@@ -122,6 +122,7 @@ public class Partie_humain {
     public String getJoueur_1() {
         return joueur_1;
     }
+
 
     /** Fonction initialisant le nom du premier joueur d'une partie entre deux humains selon la chaîne
      * de caractères passée en paramètre
@@ -132,6 +133,7 @@ public class Partie_humain {
         this.joueur_1 = j1;
     }
 
+
     /** Fonction retournant le nom du deuxième joueur d'une partie entre deux humains
      *
      * @return String correspondant au champ joueur_2 d'un objet de classe Partie_humain
@@ -139,6 +141,7 @@ public class Partie_humain {
     public String getJoueur_2() {
         return joueur_2;
     }
+
 
     /** Fonction initialisant le nom du deuxième joueur d'une partie entre deux humains selon la chaîne
      * de caractères passée en paramètre
@@ -149,6 +152,7 @@ public class Partie_humain {
         this.joueur_2 = j2;
     }
 
+
     /** Fonction retournant le nombre de colonnes de la grille d'une partie entre deux humains
      *
      * @return int correspondant au champ colonnes d'un objet de classe Partie_humain
@@ -156,6 +160,7 @@ public class Partie_humain {
     public int getColonnes() {
         return colonnes;
     }
+
 
     /** Fonction retournant le nombre de lignes de la grille d'une partie entre deux humains
      *
@@ -165,11 +170,13 @@ public class Partie_humain {
         return lignes;
     }
 
+
     /** Fonction qui retourne le numéro de la partie (qui est aussi le numéro de sauvegarde de la partie) de l'objet courant
      *
      * @return int : le numéro de la partie
      */
     public int getNumPartie(){ return this.num_partie;}
+
 
     /** Fonction initialisant le nombre de lignes de la grille d'une partie entre deux humains
      *
@@ -179,6 +186,7 @@ public class Partie_humain {
         this.lignes = l;
     }
 
+
     /** Fonction initialisant le nombre de colonnnes de la grille d'une partie entre deux humains
      *
      * @param c int correspondant au nouveau champ colonnes d'un objet de classe Partie_humain
@@ -186,6 +194,7 @@ public class Partie_humain {
     public void setColonnes(int c) {
         this.colonnes = c;
     }
+
 
     /** Fonction retournant le nombre de coups qui ont été joués dans une partie entre deux humains
      *
@@ -195,6 +204,7 @@ public class Partie_humain {
         return nb_coups;
     }
 
+
     /** Fonction initialisant le nombre de de coups qui ont été joués dans partie entre deux humains
      *
      * @param coups int correspondant au nouveau champ nb_coups d'un objet de classe Partie_humain
@@ -202,4 +212,143 @@ public class Partie_humain {
     public void setNb_coups(int coups) {
         this.nb_coups = coups;
     }
+
+
+    /** Méthode qui permet de calculer le score d'un joueur sur l'instance de Partie_humain courante
+     *
+     * @param pion int représentant le pion associé au joueur dont on calcule le score (1 pour le joueur 1, 2 pour le Joueur 2)
+     * @return int représentant le score du Joueur
+     */
+    public int score(int pion){
+        Partie_humain p = this;
+        int score = 0;
+        int[][] tab = p.tab_de_jeu;
+        //On compte les alignements verticaux
+        for (int j = 0; j < p.colonnes; j++) {
+            if (tab[p.getLignes() - 1][j] != 0) {
+                //Alignement de 4
+                int hauteur = Deroulement_partie.gravite(tab, j)-1;
+                if (hauteur >= 0){
+                    if (tab[hauteur][j] == pion) {
+                        if ((tab[hauteur][j] == tab[hauteur - 1][j]) && (tab[hauteur][j] == tab[hauteur - 2][j]) &&
+                                (tab[hauteur][j] == tab[hauteur - 3][j])) {
+                            score += 4;
+
+                            //Conditions sur la hauteur pour vérifier qu'il reste une case de libre pour faire l'alignement
+                        } else if ((hauteur < p.lignes - 1) && (tab[hauteur][j] == pion) && (tab[hauteur][j] == tab[hauteur - 1][j])
+                                && (tab[hauteur][j] == tab[hauteur - 2][j]) && (tab[hauteur - 3][j] == 0)) {
+                            score += 3;
+
+                        } else if ((hauteur < p.lignes - 2) && (tab[hauteur][j] == tab[hauteur - 1][j]) &&
+                                (tab[hauteur - 2][j] == 0) && (tab[hauteur - 3][j] == 0)) {
+                            score += 2;
+                        } else if ((hauteur < p.colonnes - 3) && (tab[hauteur - 1][j] == 0) &&
+                                (tab[hauteur - 2][j] == 0) && (tab[hauteur - 3][j] == 0)) {
+                            score += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        //Alignement horizontaux
+        int k = p.lignes - 1;
+        while (k >= 0) {
+            for (int j = 0; (j < p.colonnes) && (!Deroulement_partie.ligne_vide(tab[k])) ; j++) {
+                if (tab[k][j] == pion) {
+                    if (j < p.colonnes - 3) {
+                        if ((tab[k][j] == tab[k][j + 1]) && (tab[k][j] == tab[k][j + 2])
+                                && (tab[k][j] == tab[k][j + 3])) {
+                            score += 4;
+                        } else if ((tab[k][j] == tab[k][j + 1]) && (tab[k][j] == tab[k][j + 2])
+                                && (tab[k][j + 3] == 0)) {
+                            score += 3;
+                        } else if ((tab[k][j] == tab[k][j + 1]) && (tab[k][j + 2] == 0)
+                                && (tab[k][j + 3] == 0)) {
+                            score += 2;
+                        } else if ((tab[k][j + 1] == 0) && (tab[k][j + 2] == 0)
+                                && (tab[k][j + 3] == 0)) {
+                            score += 1;
+                        }
+                    } else if (j >= 3) {
+                        if ((tab[k][j] == tab[k][j - 1]) && (tab[k][j] == tab[k][j - 2])
+                                && (tab[k][j - 3] == 0)) {
+                            score += 3;
+                        } else if ((tab[k][j] == tab[k][j - 1]) && (tab[k][j - 2] == 0)
+                                && (tab[k][j - 3] == 0)) {
+                            score += 2;
+                        } else if ((tab[k][j - 1] == 0) && (tab[k][j - 2] == 0)
+                                && (tab[k][j - 3] == 0)) {
+                            score += 1;
+                        }
+                    }
+
+                }
+            }
+            k--;
+        }
+
+        //Alignements diagonaux en / (bas gauche vers haut droit)
+        for (int i = p.lignes - 1; i >= 3; i--){
+            for (int j = 0; j < p.colonnes-3; j++){
+                if (tab[i][j] == pion) {
+                    if ((tab[i][j] == tab[i - 1][j + 1]) && (tab[i][j] == tab[i - 2][j + 2])
+                            && (tab[i][j] == tab[i - 3][j + 3])) {
+                        score += 4;
+                    } else if ((tab[i][j] == tab[i - 1][j + 1]) && (tab[i][j] == tab[i - 2][j + 2])
+                            && (tab[i - 3][j + 3] == 0)) {
+                        score += 3;
+                    } else if ((tab[i][j] == tab[i - 1][j + 1]) && (tab[i - 2][j + 2] == 0)
+                            && (tab[i - 3][j + 3] == 0)) {
+                        score += 2;
+                    } else if ((tab[i - 1][j + 1] == 0) && (tab[i - 2][j + 2] == 0)
+                            && (tab[i - 3][j + 3] == 0)) {
+                        score += 1;
+                    }
+                }
+            }
+        }
+
+
+        //Alignements diagonaux en \
+        for (int i = p.lignes - 1; i >= 3; i--){
+            for (int j = p.colonnes-1; j >= 3; j--){
+                if (tab[i][j] == pion) {
+                    if ((tab[i][j] == tab[i - 1][j - 1]) && (tab[i][j] == tab[i - 2][j - 2])
+                            && (tab[i][j] == tab[i - 3][j - 3])) {
+                        score += 4;
+                    } else if ((tab[i][j] == tab[i - 1][j - 1]) && (tab[i][j] == tab[i - 2][j - 2])
+                            && (tab[i - 3][j - 3] == 0)) {
+                        score += 3;
+                    } else if ((tab[i][j] == tab[i - 1][j - 1]) && (tab[i - 2][j - 2] == 0)
+                            && (tab[i - 3][j - 3] == 0)) {
+                        score += 2;
+                    } else if ((tab[i - 1][j - 1] == 0) && (tab[i - 2][j - 2] == 0)
+                            && (tab[i - 3][j - 3] == 0)) {
+                        score += 1;
+                    }
+                }
+            }
+        }
+
+        return score;
+    }
+
+
+//Si besoin :
+    /*public boolean equals(Partie_humain p){
+        if((this.lignes == p.lignes) && (this.colonnes == p.colonnes) && (this.joueur_1.equals(p.joueur_1)) && (this.joueur_2.equals(p.joueur_2))
+        && (this.num_partie == p.num_partie) && (this.nb_coups == p.nb_coups)){
+            for (int i = 0; i<p.lignes; i++){
+                for(int j = 0; j<p.colonnes; j++){
+                    if(this.tab_de_jeu[i][j] != p.tab_de_jeu[i][j]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }*/
 }
