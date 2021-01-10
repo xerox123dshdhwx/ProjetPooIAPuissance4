024c2513b6681a4f74b82d5aa3537f7ecbb9b194
Boughanmi Rami <rami.boughanmi@etu.u-pec.fr>
2021-01-10 18:08:30 +0100

    Replace String_color.java

--- String_color.java
+++ String_color.java
@@ -1,12 +1,36 @@
+/**
+ * Classe qui permet de colorer l'affichage console
+ */
+
 public class String_color {
+    /**
+     * Remise à zero des couleurs
+     */
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
+
+    /**
+     * Couleur rouge
+     */
     public static final String ANSI_RED = "\u001B[31m";
+
+    /**
+     * Couleur verte
+     */
     public static final String ANSI_GREEN = "\u001B[32m";
+
+    /**
+     * Couleur jaune
+     */
     public static final String ANSI_YELLOW = "\u001B[33m";
+
+    /**
+     * Couleur bleu
+     */
     public static final String ANSI_BLUE = "\u001B[34m";
+
+    /**
+     * Couleur violette
+     */
     public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
 
 }
