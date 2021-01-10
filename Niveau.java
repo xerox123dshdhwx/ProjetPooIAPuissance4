e1ea44308c08c1f13a2d2bbb71cc29dc9f4353e8
Ait Khelifa Tanina <tanina.ait-khelifa@etu.u-pec.fr>
2021-01-10 18:09:21 +0100

    Update Niveau.java

--- Niveau.java
+++ Niveau.java
@@ -1,3 +1,20 @@
+/** Classe permettant d'énumérer et représenter les niveaux de l'IA
+ * @author Tanina AIT KHELIFA
+ * */
+
 public enum Niveau {
-    FACILE, MOYEN, DIFFICILE
+    /**
+     * Niveau le plus bas
+     */
+    FACILE,
+
+    /**
+     * Niveau intermédiaire
+     */
+    MOYEN,
+
+    /**
+     * Niveau le plus haut
+     */
+    DIFFICILE
 }
